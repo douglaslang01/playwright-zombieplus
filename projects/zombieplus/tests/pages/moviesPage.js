c9eb0492d59d06da7224f6f6a4a57376ad040732
douglaslang01 <douglas.lang@outlook.com>
2025-12-02 10:07:55 -0300

    Refatorando moviesPage e adicionando um novo teste "não deve cadastrar quando os campos obrigatórios não são preenchidos"

--- projects/zombieplus/tests/pages/moviesPage.js
+++ projects/zombieplus/tests/pages/moviesPage.js
@@ -12,27 +12,39 @@ export class MoviesPage {
         await expect(this.page).toHaveURL(/.*admin/);
     }
 
-    async create(title, overview, company, release_year) {
+    async goForm() {
         await this.page.locator('a[href$="/register"]').click();
+    }
+
+    async submit() {
+        await this.page.getByRole('button', { name: 'Cadastrar' }).click();
+    }
+
+    async create(movie) {
+        await this.goForm();
 
         // await this.page.locator('#title').fill(title);
-        await this.page.getByLabel('Titulo do filme').fill(title);
-        await this.page.locator('#overview').fill(overview);
+        await this.page.getByLabel('Titulo do filme').fill(movie.title);
+        await this.page.locator('#overview').fill(movie.overview);
 
         await this.page.locator('#select_company_id .react-select__indicator')
             .click();
 
         await this.page.locator('.react-select__option')
-            .filter({ hasText: company })
+            .filter({ hasText: movie.company })
             .click()
 
         await this.page.locator('#select_year .react-select__indicator')
             .click();
 
         await this.page.locator('.react-select__option')
-            .filter({ hasText: release_year })
+            .filter({ hasText: movie.release_year })
             .click()
 
-        await this.page.getByRole('button', { name: 'Cadastrar' }).click();
+        await this.submit();
+    }
+
+    async alertHaveText(target) {
+        await expect(this.page.locator('.alert')).toHaveText(target);
     }
 }
