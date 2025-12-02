36b7abe0a49a00f8846e0a516fa2671366fb3ae3
douglaslang01 <douglas.lang@outlook.com>
2025-12-02 10:44:36 -0300

    Incluindo o método "do" pra realizar o login

--- projects/zombieplus/tests/e2e/movies.spec.js
+++ projects/zombieplus/tests/e2e/movies.spec.js
@@ -7,19 +7,13 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
     const movie = data.create;
     await executeSQL(`DELETE FROM movies WHERE title = '${movie.title}';`);
 
-    await page.login.visit();
-    await page.login.submit('admin@zombieplus.com', 'pwd123');
-    await page.login.isLoggedIn();
-
+    await page.login.do('admin@zombieplus.com', 'pwd123');
     await page.movies.create(movie);
     await page.toast.containText('Cadastro realizado com sucesso!');
 });
 
 test('não deve cadastrar quando os campos obrigatórios não são preenchidos', async ({ page }) => {
-    await page.login.visit();
-    await page.login.submit('admin@zombieplus.com', 'pwd123');
-    await page.login.isLoggedIn();
-
+    await page.login.do('admin@zombieplus.com', 'pwd123');
     await page.movies.goForm();
     await page.movies.submit();
 
