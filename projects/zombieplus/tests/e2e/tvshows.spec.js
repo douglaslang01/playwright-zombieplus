d76869330ffc6e96403b6bfa89232224cea4501d
douglaslang01 <douglas.lang@outlook.com>
2025-12-08 14:30:32 -0300

    Adicionando o teste não deve cadastrar quando o titulo é duplicado para series

--- projects/zombieplus/tests/e2e/tvshows.spec.js
+++ projects/zombieplus/tests/e2e/tvshows.spec.js
@@ -22,3 +22,13 @@ test('deve poder remover uma serie', async ({ page, request }) => {
 
     await page.popup.haveText('Série removida com sucesso.');
 });
+
+test('não deve cadastrar quando o titulo é duplicado', async ({ page, request }) => {
+    const tvshow = data.duplicate;
+    await executeSQL(`DELETE FROM tvshows WHERE title = '${tvshow.title}';`);
+    await request.api.postTvShow(tvshow);
+
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin');
+    await page.tvshows.create(tvshow);
+    await page.popup.haveText(`O título '${tvshow.title}' já consta em nosso catálogo. Por favor, verifique se há necessidade de atualizações ou correções para este item.`);
+});
